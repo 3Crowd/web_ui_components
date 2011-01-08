4dd07972f17ad0e61b54abae3640050f4523e575
Justin Lynn <j@3crowd.com>
2011-01-07 21:42:16 -0800

    add dynamic class creation for easily creating non-colliding subclasses for testing classes that are meant to be subclassed in the API

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,16 @@ RSpec.configure do |config|
 end
 
 require 'web_ui_components'
+
+# Creates a subclass of the given class with no name, i.e.
+# not assigned to a constant
+def dynamic_anonymous_subclass class_to_subclass
+  Class.new(class_to_subclass)
+end
+
+# Creates a class, which is a subclass of the given class
+def dynamic_subclass class_to_subclass = Object, opts = {}
+  opts = { :class_location => self.class, :class_prefix => "", :class_suffix => "", :class_identifier => "DynamicClass", :class_unique_id => self.object_id }.merge(opts)
+  class_name = opts[:class_name] ? opts[:class_name] : "#{opts[:class_prefix]}#{opts[:class_identifier]}#{opts[:class_unique_id]}#{opts[:class_suffix]}"
+  opts[:class_location].const_set(class_name, dynamic_anonymous_subclass(class_to_subclass))
+end
