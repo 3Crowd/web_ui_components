136b514987ebd90af889a85b7e1552b549677583
Justin Lynn <j@3crowd.com>
2011-01-07 22:08:30 -0800

    add implementations for composed_of and composed_of?, as well as specs for those methods

--- lib/web_ui_components/core/component/component_composition.rb
+++ lib/web_ui_components/core/component/component_composition.rb
@@ -7,17 +7,33 @@ module WebUIComponents
       # to be composed of the content of other components.
       module ComponentComposition
         extend ActiveSupport::Concern
-        
+
         # Instance methods defined here will be added to the extending class's singleton class.
         module ClassMethods
-          
+
           # Specify the other components of which this module is composed
-          def composed_of *components
-            raise NotImplementedError
+          def composed_of *component_tags_to_define
+            component_tags_to_define.each do |component_tag_to_define|
+              components[component_tag_to_define] = nil
+            end
           end
-          
+
+          # Query if a component is defined
+          # @param [ Symbol ] component_tag Component tag with which to query the list of defined components
+          def composed_of? component_tag
+            components.has_key?(component_tag)
+          end
+
+          private
+
+          # A hash of the current components in this component
+          # @private
+          def components
+            @components ||= Hash.new
+          end
+
         end
-        
+
       end
     end
   end
--- spec/web_ui_components/core/component/component_composition_spec.rb
+++ spec/web_ui_components/core/component/component_composition_spec.rb
@@ -30,8 +30,54 @@ describe WebUIComponents::Core::Component::ComponentComposition do
         WebUIComponents::Core::Component::ComponentComposition::ClassMethods.instance_methods.should include(:composed_of)
       end
 
-      it "adds child components to the component" do
-        pending 'implementation'
+      context "with a single argument" do
+
+        it "adds specified component to the component" do
+          component = dynamic_subclass(WebUIComponents::Core::Component, :class_suffix => 'Component')
+          component.class_eval do
+            composed_of :tabs
+          end
+          component.send(:components).should have_key(:tabs)
+        end
+
+      end
+
+      context "with multiple arguments" do
+
+        it "adds all specified components to the component" do
+          component = dynamic_subclass(WebUIComponents::Core::Component, :class_suffix => 'Component')
+          composed_of_components = [:tabs, :heads, :frets, :notes]
+          component.class_eval do
+            composed_of *composed_of_components
+          end
+          composed_of_components.each do |composed_of_component|
+            component.send(:components).should have_key(composed_of_component)
+          end
+        end
+
+      end
+
+    end
+
+    describe "method composed_of?" do
+
+      before do
+        @component = dynamic_subclass(WebUIComponents::Core::Component, :class_suffix => 'Component')
+        @component.class_eval do
+          composed_of :tags
+        end
+      end
+
+      it "is defined" do
+        WebUIComponents::Core::Component::ComponentComposition::ClassMethods.instance_methods.should include(:composed_of?)
+      end
+
+      it "returns true if a component is defined as being composed_of the given component" do
+        @component.should be_composed_of(:tags)
+      end
+
+      it "returns false if a component is not defined as being composed_of the given component" do
+        @component.should_not be_composed_of(:gluons)
       end
 
     end
