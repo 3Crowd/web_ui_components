2b9711a76388e5e647b098712776b07c92102463
Justin Lynn <j@3crowd.com>
2011-01-11 18:06:33 -0800

    we need building block sets to help specify different basic formats for variants

--- lib/web_ui_components/core/component/building_blocks.rb
+++ lib/web_ui_components/core/component/building_blocks.rb
@@ -13,13 +13,24 @@ module WebUIComponents
         module ClassMethods
 
           # Specify the building blocks that make up the content of this component
-          def building_blocks
-            @building_blocks_specified = true
+          def building_blocks set_name = :default, &building_block_set
+            building_blocks_store[set_name] = building_block_set
           end
 
           # Has this component had building blocks defined?
           def has_building_blocks?
-            !!@building_blocks_specified
+            !building_blocks_store.empty?
+          end
+
+          # Has this component had a specified named building block set defined?
+          def has_building_block_set? set_name = :default
+            !building_blocks_store[set_name].nil?
+          end
+
+          private
+
+          def building_blocks_store
+            @building_bocks ||= Hash.new
           end
 
         end
--- spec/web_ui_components/core/component/building_blocks_spec.rb
+++ spec/web_ui_components/core/component/building_blocks_spec.rb
@@ -19,35 +19,100 @@ describe WebUIComponents::Core::Component::BuildingBlocks do
     describe "class methods" do
 
       describe "method has_building_blocks?" do
-        
+
         before do
           @component = dynamic_subclass(WebUIComponents::Core::Component, :class_suffix => 'Component')
         end
-        
+
         it "is defined" do
           @component.should respond_to(:has_building_blocks?)
         end
 
-        it "indicates whether or not building blocks have been specified for the component" do
-          @component.should_not have_building_blocks
-          @component.class_eval do
-            building_blocks do
+        context "with no arguments" do
+
+          it "returns true if default building blocks have been specified for the component" do
+            @component.class_eval do
+              building_blocks do
+              end
             end
+            @component.should have_building_blocks
           end
-          @component.should have_building_blocks
+
+          it "returns false if default building blocks have not be specified for the component" do
+            @component.should_not have_building_blocks
+          end
+
+        end
+
+      end
+
+      describe "method has_building_block_set?" do
+
+        before do
+          @component = dynamic_subclass(WebUIComponents::Core::Component, :class_suffix => 'Component')
+        end
+
+        it "is defined" do
+          @component.should respond_to(:has_building_block_set?)
+        end
+
+        context "with no arguments" do
+
+          it "returns true if the default building block set is defined" do
+            @component.class_eval do
+              building_blocks{}
+            end
+            @component.should have_building_block_set(:default)
+          end
+
+          it "returns false if the default building block set is not defined" do
+            @component.should_not have_building_block_set(:default)
+          end
+
+        end
+
+        context "with one argument specifying the set name" do
+
+          it "returns true if the specified building block set is defined" do
+            @component.class_eval do
+              building_blocks :test_block_set do
+              end
+            end
+            @component.should have_building_block_set(:test_block_set)
+          end
+
+          it "returns false if the specified building block set is not defined" do
+            @component.should_not have_building_block_set(:test_block_set)
+          end
+
         end
 
       end
 
       describe "method building_blocks" do
 
+        before do
+          @component = dynamic_subclass(WebUIComponents::Core::Component, :class_suffix => 'Component')
+        end
+
         it "is defined" do
           WebUIComponents::Core::Component::BuildingBlocks::ClassMethods.instance_methods.should include(:building_blocks)
         end
 
-        it "adds building blocks to the content for the component" do
-          pending 'implementation'
+        it "should not contain any building block sets if none are specified" do
+          @component.should_not have_building_blocks
         end
+
+        context "with no arguments, and passed a block" do
+
+          it "adds building blocks to the content for the component using the :default building_block set name" do
+            @component.class_eval do
+              building_blocks{}
+            end
+            @component.should have_building_block_set(:default)
+          end
+        end
+
 
       end
 
