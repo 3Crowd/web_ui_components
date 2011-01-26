362c23e2bbee707d78ea6e5b9051cc5da2243635
Justin Lynn <justinlynn@gmail.com>
2011-01-26 00:38:38 -0800

    allow naming of building blocks

--- lib/web_ui_components/core/building_blocks/building_block.rb
+++ lib/web_ui_components/core/building_blocks/building_block.rb
@@ -4,6 +4,25 @@ module WebUIComponents
       # The class from which all BuildingBlocks are descended
       # @private
       class BuildingBlock
+        
+        class << self
+          
+          # Adds a given name or names to the list of names by which this building_block
+          # may be referred to within the component building_blocks method
+          def component_name *names_to_add
+            names_to_add.each do |name_to_add|
+              component_names << name_to_add unless component_names.member?(name_to_add)
+            end
+          end
+          
+          # Returns an array of the component names assigned to this building block for use
+          # within the component building_blocks method
+          def component_names
+            @component_names ||= []
+          end
+          
+        end
+        
       end
     end
   end
--- spec/web_ui_components/core/building_blocks/building_block_spec.rb
+++ spec/web_ui_components/core/building_blocks/building_block_spec.rb
@@ -15,6 +15,64 @@ describe WebUIComponents::Core::BuildingBlocks::BuildingBlock do
   end
   
   describe 'class methods' do
+    
+    describe 'method component_name' do
+      
+      before do
+        @building_block = dynamic_subclass(WebUIComponents::Core::BuildingBlocks::BuildingBlock)
+      end
+      
+      context 'with a single argument representing a single component_name' do
+        
+        it 'assigns the block a name for use inside component building block methods' do
+          test_building_block_name = :test_building_block
+          @building_block.component_names.should be_empty
+          @building_block.class_eval do
+            component_name test_building_block_name
+          end
+          @building_block.component_names.should include(test_building_block_name)
+        end
+        
+      end
+      
+      context 'with multiple arguments representing a collection of component_names' do
+        
+        it 'assigns the block multiple names for use inside component building block methods' do
+          test_building_block_names = [ :test_building_block, :test_building_block_1, :test_building_block_2 ]
+          @building_block.component_names.should be_empty
+          @building_block.class_eval do
+            component_name *test_building_block_names
+          end
+          test_building_block_names.each do |building_block_name|
+            @building_block.component_names.should include(building_block_name)
+          end
+        end
+        
+      end
+      
+    end
+    
+    describe 'method component_names' do
+      
+      before do
+        @building_block = dynamic_subclass(WebUIComponents::Core::BuildingBlocks::BuildingBlock)
+      end
+      
+      context 'with no arguments' do
+        
+        it 'returns a list of the registered names by which this building_block can be referred to from within a component' do
+          test_component_names = [:test_component, :test_component_1]
+          @building_block.component_names.should be_empty
+          @building_block.class_eval do
+            component_name *test_component_names
+          end
+          @building_block.component_names.should == test_component_names
+        end
+        
+      end
+      
+    end
+    
   end
   
 end
